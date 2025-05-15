5e2c88aa7e41a7901e4c5a7f2ea53e910fefadd3
Shubham <shubhamsolat51@gmail.com>
2025-05-15 17:28:31 +0530

    theme basic setup done

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Homepage() {
+  const [theme, setTheme] = useState(localStorage.getItem("currentTheme") || "dark-theme");
+
+  const toggleTheme = () => {
+    const newTheme = theme === "dark-theme" ? "light-theme" : "dark-theme";
+
+    setTheme(newTheme);
+    localStorage.setItem("currentTheme", newTheme);
+  };
+
   return (
-    <div className="w-screen h-screen flex items-center justify-center">
-      <div className="text-red-500 font-bold">Homepage</div>
+    <div className={`flex items-center justify-center w-screen h-screen gap-4 bg-1 ${theme}`}>
+      <div className="font-bold text-color-1">Homepage</div>
+      <button onClick={toggleTheme}>Toggle Theme</button>
     </div>
   );
 }
